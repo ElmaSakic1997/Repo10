2be3ebd687c759ee443b0cf2c15eee821448456b
ElmaSakic1997 <elma.sakic97@gmail.com>
2021-09-22 10:44:24 +0200

    Objects

---
+++ ex01/src/index.js
@@ -0,0 +1,15 @@
+// Only change code below this line
+var myPet = {
+    species: "Dog",
+    name: "Micy",
+    legs: 4,
+    friends: ["Rex", "Bella"]
+}
+
+function myFunction(obj) {
+    return obj;
+}
+// Only change code above this line
+
+console.log(myFunction(myPet)); // Change this line
+module.exports = { myPet, myFunction };
